6e1ddc95cfd2274f8e30aff010c0c51d9169d53f
Mael <sasorith.mael@gmail.com>
2025-11-25 14:37:37 +0100

    MAJ

--- TP4/Exercice 2.py
+++ TP4/Exercice 2.py
@@ -5,7 +5,6 @@ for i in range (nombreEtudiants) :
     notes.append(float(input(f"La note de l'èleve est : {i + 1}")))
 moy = sum(notes) / nombreEtudiants
 print(f"La moyenne de la classe est : {moy}")
-x = moy - notes
-print(f"{i + 1} | {notes} | {x}")
-
+ecar = moy - notes
+print(f"{i + 1} | {notes} | {ecar}")
 
