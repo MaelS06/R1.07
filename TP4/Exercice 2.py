9ac1b03014e243b20c47f020e497a9eeacdc04e8
Mael <sasorith.mael@gmail.com>
2025-11-25 15:36:39 +0100

    MAJ

--- TP4/Exercice 2.py
+++ TP4/Exercice 2.py
@@ -1,10 +1,19 @@
 nombreEtudiants = int(input("Donnez le nombre d'etudiants : "))
-moyenne = 0.0
+moy = 0.0
 notes = []
-for i in range (nombreEtudiants) :
-    notes.append(float(input(f"La note de l'èleve est : {i + 1}")))
-moy = sum(notes) / nombreEtudiants
-print(f"La moyenne de la classe est : {moy}")
-ecar = moy - notes
-print(f"{i + 1} | {notes} | {ecar}")
+for i in range(nombreEtudiants):
+    note = float(input(f"Note étudiant {i+1} : "))
+    while note < 0 or note > 20:
+        print("Erreur : une note doit être comprise entre 0 et 20 !")
+        note = float(input(f"Note etudiant {i} : "))
+    notes.append(note)
+    moy = moy + note
 
+moy = moy / nombreEtudiants
+
+print(f"Moyenne de classe : {moy}")
+
+print("Numéro de l’Etudiant | note | ecart a la moyenne")
+for i in range(nombreEtudiants):
+    ecart = notes[i] - moy
+    print(f"{i} | {notes[i]} | {round(ecart, 2)}")
